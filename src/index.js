79f2c7b0169f74d63a02b6582d253f01ba7e0d20
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2022-08-22 19:33:11 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import ReactDOM from 'react-dom';
-import { Router } from "react-router-dom";
-import "./assets/style.css";
-import "./assets/responsive.css";
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/home.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import ApodContainer from "../containers/ApodContainer/index";
-
-class Home extends Component {
-  render() {
-    return <ApodContainer />;
-  }
-}
-export default Home;
