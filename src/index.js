7615079bd4f32afaeb2870806d3acce76fbcdd33
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2022-08-22 18:11:23 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+import { Router } from "react-router-dom";
+import "./assets/style.css";
+import "./assets/responsive.css";
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
